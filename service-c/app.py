4899c8f81352c7bc860cf722fd4dd7c32501794e
kia <kia@dev.local>
2026-03-02 17:45:48 +0530

    malware

--- service-c/app.py
+++ service-c/app.py
@@ -1,3 +1,4 @@
+import os
 from fastapi import FastAPI, Depends, HTTPException
 from fastapi.security import HTTPBasic, HTTPBasicCredentials
 
@@ -6,8 +7,10 @@ app = FastAPI()
 security = HTTPBasic()
 
 def authenticate(credentials: HTTPBasicCredentials = Depends(security)):
-    valid_username = "admin"
-    valid_password = "password123"
+    valid_username = os.getenv("VALID_USERNAME")
+    valid_password = os.getenv("VALID_PASSWORD")
+    if not valid_username or not valid_password:
+        raise HTTPException(status_code=500, detail="Server misconfiguration: Missing credentials")
     if credentials.username != valid_username or credentials.password != valid_password:
         raise HTTPException(status_code=401, detail="Unauthorized")
 
